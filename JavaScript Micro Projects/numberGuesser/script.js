9a21951a3829daf11d99abe5dfa66226a3c29444
Rockysd3 <73887275+Rockysd3@users.noreply.github.com>
2021-08-08 01:51:31 +0200

    Update script.js

--- JavaScript Micro Projects/numberGuesser/script.js
+++ JavaScript Micro Projects/numberGuesser/script.js
@@ -1,20 +1,25 @@
-// very basic project for a number guessing game with basic graphics
-
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
 generateTarget=()=>{
   return Math.floor(Math.random()*9);
 }
+
 compareGuesses=(human,comp,secret)=>{ 
-if (((Math.abs(human-secret)) > (Math.abs(comp-secret))) || ((Math.abs(human-secret)) === (Math.abs(comp-secret)))){
+if ( getAbsoluteDistance(human,secret) > getAbsoluteDistance(comp,secret) ){
+return false;
+} else if (getAbsoluteDistance(human,secret) < getAbsoluteDistance(comp,secret)){
 return true;
-} else {
-  return false;
+} else if ( getAvsoluteDistance(human,secret) === getAbsoluteDistance(comp,secret) ){
+  return true;
 }
 }
+
+getAbsoluteDistance=(num1,num2)=>{
+return Math.abs(num1-num2);
+}
+
 updateScore=(winner)=>{
 if (winner='human'){
   humanScore++;
@@ -22,6 +27,13 @@ if (winner='human'){
   computerScore++;
 }
 }
+
 advanceRound=()=>{
 currentRoundNumber++;
+}
+
+alert=()=>{
+  if (human > 9 || human< 0){
+    return 'you entered a wrong number, please enter from zero till nine'
+  }
 }
