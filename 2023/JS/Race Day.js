4c3da591e9f3404b73f2a0781ad99647ece3fcc5
ali.hisham3355 <ali.hisham3355@gmail.com>
2023-11-09 19:27:34 +0100

    initial

---
+++ 2023/JS/Race Day.js
@@ -0,0 +1,15 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let regStatusEarly= false;
+let runnerAge=18;
+
+if (runnerAge >= 18 && regStatusEarly === true){
+  raceNumber +=1000;
+  console.log (`race starts at 9:30 am and your number is ${raceNumber}`);
+  } 
+  
+else if (runnerAge >= 18 && regStatusEarly !== true) {
+  console.log (`race starts at 11:00 am and your number is ${raceNumber}`);
+} 
+else if (runnerAge < 18) {
+  console.log (`race starts at 12:30 pm and your number is ${raceNumber}`);
+}
